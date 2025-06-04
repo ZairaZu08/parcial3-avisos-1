6bf3aa8b38f9f96a3558c432a36539432b0d6b8e
miacastillo22 <miaadyereren.castillogamez.gen23@cbtis105.edu.mx>
2025-06-04 09:34:31 -0600

    terminamos

--- Controllers/Api/MiProyectoController.cs
+++ Controllers/Api/MiProyectoController.cs
@@ -1,24 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
-using MongoDB.Bson;
+using MongoDB.Driver;
 using Parcial3Avisos.Models; 
 
 namespace Parcial3Avisos.Controllers.Api
 {
     [ApiController]
-    [Route("api/[controller]")]
-    public class MiProyectoController : ControllerBase
-    {
+    [Route("mi-proyecto")]
+    public class MiProyectoController : ControllerBase {
         [HttpGet("integrantes")]
-        public ActionResult<MiProyecto> Integrantes()
-        {
-            var proyecto = new MiProyecto
-            {
-                Id = ObjectId.GenerateNewId().ToString(),
-                NombreIntegrante1 = "Mia Adyeren Castillo Gamez",
-                NombreIntegrante2 = "Danna Paola Castillo Briales"
-            };
+        public IActionResult Integrantes() {
+           MiProyecto mi = new MiProyecto();
+           mi.NombreIntegrante1 = "Mia Adyeren Castillo Gamez";
+           mi.NombreIntegrante2 = "Danna Paola Castillo Briales";
 
-            return Ok(proyecto);
+           return Ok (mi);
+        }
+
+        [HttpGet("presentacion")]
+        public IActionResult Presentacion() {
+            var client = new MongoClient(CadenasConexion.Mongo_DB);
+            var bd = client.GetDatabase("Escuela_Mia_Danna");
+            var collection = bd.GetCollection<Equipo>("Equipo");
+            var list = collection.Find(FilterDefinition<Equipo>.Empty).ToList();
+
+           return Ok (list);
         }
     }
+    
 }
