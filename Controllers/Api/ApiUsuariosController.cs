327753c3200eb495ab4e2c63da6e67dadbcde187
miacastillo22 <miaadyereren.castillogamez.gen23@cbtis105.edu.mx>
2025-06-18 10:03:03 -0600

    parte 3 continuacion 3

--- Controllers/Api/ApiUsuariosController.cs
+++ Controllers/Api/ApiUsuariosController.cs
@@ -1,23 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 using MongoDB.Driver;
-using Parcial3Avisos.Models;
-using MongoDB.Bson; 
 
 [ApiController]
 [Route("api/usuarios")]
 public class ApiUsuariosController : ControllerBase
 {
-    //Métodos para hacer las operacines CRUD
-    //C= Create
-    //R= Read
-    //U= Update
-    //D= Delete
-    
+    //Metodos para hacer operaciones CRUD
+    // C = Create
+    // R= Read
+    // U= Update
+    // D= Delete
+
     private readonly IMongoCollection<Usuario> collection;
     public ApiUsuariosController()
     {
-        var client = new MongoClient(CadenasConexion.Mongo_DB);
-        var database = client.GetDatabase("Escuela_Mia_Danna");
+        var client = new MongoClient(CadenaConexion.MONGO_DB);
+        var database = client.GetDatabase("Escuela_Zaira_Atala");
         this.collection = database.GetCollection<Usuario>("Usuarios");
     }
 
@@ -27,25 +26,27 @@ public class ApiUsuariosController : ControllerBase
         var filter = FilterDefinition<Usuario>.Empty;
         if (!string.IsNullOrWhiteSpace(texto))
         {
-            var filterNombre = Builders<Usuario>.Filter.Regex(u => u.Nombre, new BsonRegularExpression(texto,"i"));
-            var filterCorreo = Builders<Usuario>.Filter.Regex(u => u.Correo, new BsonRegularExpression(texto,"i"));
+            var filterNombre = Builders<Usuario>.Filter.Regex(X => X.Nombre, new BsonRegularExpression(texto, "i"));
+            var filterCorreo = Builders<Usuario>.Filter.Regex(X => X.Correo, new BsonRegularExpression(texto, "i"));
             filter = Builders<Usuario>.Filter.Or(filterNombre, filterCorreo);
         }
 
         var list = this.collection.Find(filter).ToList();
+
         return Ok(list);
     }
-    [HttpGet("{id}")]
-    public IActionResult Read(string id)
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(string id)
     {
         var filter = Builders<Usuario>.Filter.Eq(x => x.Id, id);
         var item = this.collection.Find(filter).FirstOrDefault();
-        if (item == null)
+        if (item != null)
         {
-            return NotFound("No existe un usuario con el ID proporcionando");
+            this.collection.DeleteOne(filter);
         }
 
-        return Ok(item);
+        return NoContent();
     }
 
     [HttpPost]
@@ -56,29 +57,92 @@ public class ApiUsuariosController : ControllerBase
         {
             return BadRequest("El correo es requerido");
         }
+
         if (string.IsNullOrWhiteSpace(model.Password))
         {
             return BadRequest("El password es requerido");
         }
+
         if (string.IsNullOrWhiteSpace(model.Nombre))
         {
             return BadRequest("El nombre es requerido");
         }
 
-        //2.Validar que el correo no exista
+        //Validar que el correo no exista
         var filter = Builders<Usuario>.Filter.Eq(x => x.Correo, model.Correo);
         var item = this.collection.Find(filter).FirstOrDefault();
         if (item != null)
         {
             return BadRequest("El correo " + model.Correo + " ya existe en la base de datos");
-        } 
+        }
+
         Usuario bd = new Usuario();
         bd.Nombre = model.Nombre;
         bd.Correo = model.Correo;
         bd.Password = model.Password;
 
         this.collection.InsertOne(bd);
-        
         return Ok();
     }
-}
+
+    [HttpGet("{id}")]
+    public IActionResult Read(string id)
+    {
+        var filter = Builders<Usuario>.Filter.Eq(x => x.Id, id);
+        var item = this.collection.Find(filter).FirstOrDefault();
+        if (item == null)
+        {
+            return NotFound("No existe un usuario con el ID proporcionado");
+        }
+
+        return Ok(item);
+    }
+
+         [HttpPut("{id}")]
+        public IActionResult Update(string id, UsuarioRequest model)
+        {
+            //1.Validar el modelo para que contenga datos
+            if(string.IsNullOrWhiteSpace(model.Correo))
+            {
+                return BadRequest("El correo es requerido");
+            }
+
+            if(string.IsNullOrWhiteSpace(model.Password))
+            {
+                return BadRequest("El password es requerido");
+            }
+
+            if(string.IsNullOrWhiteSpace(model.Nombre))
+            {
+                return BadRequest("El nombre es requerido");
+            }
+            
+            var filter = Builders<Usuario>.Filter.Eq(x => x.Id, id);
+            var item = this.collection.Find(filter).FirstOrDefault();
+            if (item == null)
+        {
+            return NotFound("No existe un usuario con el ID proporcionado");
+        }
+
+
+            //Validar que el correo no exista
+            var filterCorreo = Builders<Usuario>.Filter.Eq(x => x.Correo, model.Correo);
+            var itemExistente = this.collection.Find(filterCorreo).FirstOrDefault();
+            Console.Write(model.Correo);
+            Console.Write(itemExistente.Id);
+            Console.Write(id);
+            if(itemExistente != null && itemExistente.Id != id)
+            {
+                return BadRequest("El correo " + model.Correo + " ya existe en la base de datos");
+            }
+
+            var updateOptions = Builders<Usuario>.Update
+            .Set(x => x.Correo, model.Correo)
+            .Set(x => x.Nombre, model.Nombre)
+            .Set(x => x.Password, model.Password);
+
+            this.collection.UpdateOne(filter, updateOptions);
+
+            return Ok();
+        }
+    }
